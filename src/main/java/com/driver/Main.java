e9a636bda4eeae984188068b9b1c166534995b28
sriram viswanathan <sriramviswanathan2@gmail.com>
2023-01-16 15:09:00 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+        B b = new B();
+        b.meth();
+        b.meth();
+    }
 }
